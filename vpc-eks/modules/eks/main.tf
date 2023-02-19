c52c39887e947ac3aa5c6878faea2316a6643a48
Boris Quiroz <bmquiroz@outlook.com>
2023-02-19 17:42:48 -0500

    ...

--- vpc-eks/modules/eks/main.tf
+++ vpc-eks/modules/eks/main.tf
@@ -54,7 +54,7 @@ resource "aws_iam_role" "eks" {
 POLICY
 }
 
-resource "aws_iam_role" "eks_node" {
+resource "aws_iam_role" "eks-node" {
   name = lower("${var.application}-${var.uai}-${lookup(var.tagging_standard, "deployment")}-eks-node")
   assume_role_policy = jsonencode({
     Statement = [{
@@ -66,6 +66,32 @@ resource "aws_iam_role" "eks_node" {
     }]
     Version = "2012-10-17"
   })
+}
+
+resource "aws_iam_role" "eks-fargate-pod-execution" {
+  name = lower("${var.application}-${var.uai}-${lookup(var.tagging_standard, "deployment")}-eks-fargate-pod-execution")
+  assume_role_policy = jsonencode({
+    Statement: [
+        {
+            Effect: "Allow",
+            Condition: {
+                ArnLike: {
+                    "aws:SourceArn": "arn:aws:eks:region-code:795345444371:fargateprofile/*"
+                }
+            },
+            Principal: {
+                Service: "eks-fargate-pods.amazonaws.com"
+            },
+            Action: "sts:AssumeRole"
+        }
+    ]
+    Version = "2012-10-17"
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks-fargate-pod-execution-policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSFargatePodExecutionRolePolicy"
+  role       = aws_iam_role.eks-fargate-pod-execution.name
 }
 
 resource "aws_iam_role_policy_attachment" "eks-cluster-policy" {
@@ -90,39 +116,54 @@ resource "aws_iam_role_policy_attachment" "container-registry-policy" {
 
 resource "aws_iam_role_policy_attachment" "eks-cni-policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = aws_iam_role.eks_node.name
+  role       = aws_iam_role.eks-node.name
 }
 
 resource "aws_iam_role_policy_attachment" "ec2-container-registry-readonly-policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = aws_iam_role.eks_node.name
+  role       = aws_iam_role.eks-node.name
 }
 
-resource "aws_eks_node_group" "eks-node-group" {
-  cluster_name    = aws_eks_cluster.eks.name
-  node_group_name = lower("${var.application}-${var.uai}-${lookup(var.tagging_standard, "deployment")}-eks-nodegroup")
-  node_role_arn   = aws_iam_role.eks.arn
-  subnet_ids      = var.subnet_ids
-  instance_types  = [var.instance_type]
+# resource "aws_eks_node_group" "eks-node-group" {
+#   cluster_name    = aws_eks_cluster.eks.name
+#   node_group_name = lower("${var.application}-${var.uai}-${lookup(var.tagging_standard, "deployment")}-eks-nodegroup")
+#   node_role_arn   = aws_iam_role.eks.arn
+#   subnet_ids      = var.subnet_ids
+#   instance_types  = [var.instance_type]
 
-  scaling_config {
-    desired_size = 4
-    max_size     = 5
-    min_size     = 2
+#   scaling_config {
+#     desired_size = 4
+#     max_size     = 5
+#     min_size     = 2
+#   }
+
+#   update_config {
+#     max_unavailable = 2
+#   }
+
+#   tags = merge(
+#     var.tagging_standard,
+#     tomap({"Name" = "${var.application}-${var.uai}-${lookup(var.tagging_standard, "deployment")}-eks-nodegroup"})
+#   )
+
+#   depends_on = [
+#     aws_iam_role_policy_attachment.eks-worker-node-policy,
+#     aws_iam_role_policy_attachment.eks-cni-policy,
+#     aws_iam_role_policy_attachment.ec2-container-registry-readonly-policy,
+#   ]
+# }
+
+resource "aws_eks_fargate_profile" "eks-fargate-profile" {
+  cluster_name           = aws_eks_cluster.eks.name
+  fargate_profile_name   = lower("${var.application}-${var.uai}-${lookup(var.tagging_standard, "deployment")}-eks-fargate")
+  pod_execution_role_arn = aws_iam_role.eks-fargate-pod-execution.arn
+  subnet_ids             = var.subnet_ids
+
+  selector {
+    namespace = "awx"
   }
 
-  update_config {
-    max_unavailable = 2
-  }
-
-  tags = merge(
-    var.tagging_standard,
-    tomap({"Name" = "${var.application}-${var.uai}-${lookup(var.tagging_standard, "deployment")}-eks-nodegroup"})
-  )
-
-  depends_on = [
-    aws_iam_role_policy_attachment.eks-worker-node-policy,
-    aws_iam_role_policy_attachment.eks-cni-policy,
-    aws_iam_role_policy_attachment.ec2-container-registry-readonly-policy,
+    depends_on = [
+    aws_iam_role_policy_attachment.eks-fargate-pod-execution-policy,
   ]
 }
